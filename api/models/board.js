5a38bd867e67fa971c7076613c297e2574907f6a
Matteus Legat <matteus@legat.ml>
2019-09-14 12:42:13 -0300

    Implement pieces placing on the board

--- api/models/board.js
+++ api/models/board.js
@@ -1,17 +1,30 @@
+import uuid from 'uuid/v4'
+
+// Chess Board Model
+
+// Constant board properties
+const height = 8
+const width = 8
+const validSets = Object.freeze([ 'black', 'white' ])
+const maxPieces = {
+  knight: 1,
+}
+
 class Board {
 
-  // Initilize board and properties:
+  // Initilize board
   constructor() {
-    this.validPieces = [
-      { name: 'knight', max: 1 },
-    ]
-    this.height = 8
-    this.width = 8
-    this.placedPieces = []
+    this.placedPieces = {/* id: {id, type, set, x, y} */}
   }
 
+  // Constant getters
+  static get height() { return height }
+  static get width() { return width }
+  static get maxPieces() { return maxPieces }
+  static get validSets() { return validSets }
+
   // Parse position from Algebraic Notation (e.g. 'A5') to Array Position (e.g. [0, 4]) 
-  parseToArrPosition(algPos) {
+  static parseToArrPosition(algPos) {
     const fullPattern = /^[A-Z][0-9]+$/s
     algPos = algPos.toUpperCase()
 
@@ -29,17 +42,51 @@ class Board {
   }
 
   // Parse position from Array Position (e.g. [0, 4]) to Algebraic Notation (e.g. 'A5')
-  parseToAlgNotation(arrPos) {
+  static parseToAlgNotation(arrPos) {
     const [x, y] = arrPos
     return String.fromCharCode('A'.charCodeAt(0) + x) + (y+1)
   }
 
   // Check if array position is valid on the board
-  validatePosition(arrPos) {
+  static validatePosition(arrPos) {
     const [x, y] = arrPos
     return (x >= 0 && x < this.width) && (y >= 0 && y < this.height)
   }
 
+  // Check if array position is taken by some piece
+  positionIsTaken(arrPos) {
+    const [x, y] = arrPos
+    return this.placedPieces.some(piece => piece.x == x && piece.y == y)
+  }
+
+  // Place new piece on the board
+  placePiece(type, algPos, set) {
+    if (!maxPieces.type) {
+      throw "Invalid Piece Type"
+    }
+
+    if (!validSets.includes(set)) {
+      throw "Invalid Piece Set"
+    }
+
+    const similarPieces = this.placedPieces.filter(
+      piece => (piece.type == type && piece.set == set)
+    )
+    if (maxPieces.type <= similarPieces.lenght) {
+      throw "Exceeded pieces of same type and set"
+    }
+
+    const [x, y] = this.parseToArrPosition(algPos)
+    
+    if (this.positionIsTaken()) {
+      throw "Position already taken"
+    }
+
+    const newPiece = { id: uuid(), type, set, x, y }
+    this.placedPieces[newPiece.id]
+
+    return newPiece
+  }
 }
 
 export default Board
